f376de5bed06b6497a743eabc5cd6cb0f82ad1c4
redondo <riad.reda.fethi@gmail.com>
2025-08-03 15:16:51 +0200

    ajout de fichier de conexion bdd et modif index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from backend!");
-});
+app.use(cors());
+app.use(express.json());
 
+// Routes
+app.use('/api/auth', require('./routes/auth.routes'));
+
+// Démarrage serveur
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Backend is running on http://localhost:${PORT}`);
+  console.log(`Serveur lancé sur le port ${PORT}`);
 });
